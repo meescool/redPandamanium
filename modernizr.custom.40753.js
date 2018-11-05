7d30264cffa6ae419ba1b82a35a3b5c749aea148
meescool <43302795+meescool@users.noreply.github.com>
2018-11-05 15:43:10 -0600

    Files added
    I uploaded the current state of the website!

---
+++ modernizr.custom.40753.js
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>URL</key>
+	<string>https://filr.wsc.edu/ssf/s/readFile/folderEntry/12160065/8ad08a4a61bec6230162458a503035b4/1503505826000/last/modernizr.custom.40753.js</string>
+</dict>
+</plist>
